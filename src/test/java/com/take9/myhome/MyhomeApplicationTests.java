816f15e26086a165227ab63dc694661933f2f36d
decur <coffee5158@naver.com>
2020-07-03 17:07:04 +0900

    추가

---
+++ src/test/java/com/take9/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.take9.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
